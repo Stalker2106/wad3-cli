1c4c2cd258dd0500cee8485a87aa92c93eaac61c
Stalker2106 <contact@maximemartens.fr>
2024-11-13 19:06:44 +0100

    moved to x-platform libs

---
+++ ColorPalette.cs
@@ -0,0 +1,49 @@
+﻿using SkiaSharp;
+
+namespace wad3_cli
+{
+    internal class ColorPalette
+    {
+        SKColor[] colors;
+        Dictionary<string, int> assoc;
+        int currentIndex;
+
+        public ColorPalette(int paletteSize = 256)
+        {
+            colors = new SKColor[paletteSize];
+            assoc = new Dictionary<string, int>();
+            currentIndex = 0;
+        }
+
+        public int GetOrAddPaletteIndex(SKColor color)
+        {
+            string colorName = color.Red.ToString("000") + color.Green.ToString("000") + color.Blue.ToString("000");
+            if (!assoc.ContainsKey(colorName))
+            {
+                colors[currentIndex] = color;
+                assoc[colorName] = currentIndex;
+                currentIndex += 1;
+            }
+            return assoc[colorName];
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] paletteData = new byte[colors.Length * 3];
+            int paletteIndex = 0;
+            foreach (SKColor color in colors)
+            {
+                paletteData[paletteIndex] = color.Red;
+                paletteData[paletteIndex + 1] = color.Green;
+                paletteData[paletteIndex + 2] = color.Blue;
+                paletteIndex += 3;
+            }
+            // Pad missing bytes
+            while (paletteIndex < paletteData.Length)
+            {
+                paletteData[paletteIndex] = 0;
+            }
+            return paletteData;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿using Sledge.Formats.Texture.Wad;
 using Sledge.Formats.Texture.Wad.Lumps;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.CommandLine;
+using SkiaSharp;
+using KGySoft.Drawing.SkiaSharp;
+using KGySoft.Drawing.Imaging;
+using Sledge.Formats.Id;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.IO;
 
-namespace Wad3CLI
+namespace wad3_cli
 {
     class Program
     {
@@ -12,9 +17,9 @@ namespace Wad3CLI
         {
             var rootCommand = new RootCommand("GoldSrc WAD3 CLI utility");
 
-            var bundleCommand = new Command("bundle", "Bundles various bitmaps into a WAD3 file");
+            var bundleCommand = new Command("bundle", "Bundles multiple textures into a WAD3 file");
             rootCommand.AddCommand(bundleCommand);
-            var bundleSourceArgument = new Argument<string>("sourceFolder", "Path to folder to scan for source Bitmaps");
+            var bundleSourceArgument = new Argument<string>("sourceFolder", "Path to folder to scan for source textures");
             var bundleOutputArgument = new Argument<string>("outputWAD", "Path to WAD file to create");
             var bundleRecursiveOption = new Option<bool>("--recursive", "Walk the sourceFolder recursively");
             bundleCommand.AddArgument(bundleSourceArgument);
@@ -25,14 +30,16 @@ namespace Wad3CLI
             {
                 var searchOptions = SearchOption.TopDirectoryOnly;
                 if (recursive) searchOptions = SearchOption.AllDirectories;
-                string[] bitmapsPath = Directory.GetFiles(sourceFolder, "*.bmp", searchOptions).ToArray();
+                string[] bitmapsPath = Directory.GetFiles(sourceFolder, "*.*", searchOptions).ToArray()
+                                                .Where(file => new string[] { ".bmp", ".jpg", ".gif", ".png" }
+                                                .Contains(Path.GetExtension(file))).ToArray();
                 if (Bundle(bitmapsPath, outputWAD))
                 {
                     Console.WriteLine($"WAD successfully generated at {outputWAD}");
                 }
             }, bundleSourceArgument, bundleOutputArgument, bundleRecursiveOption);
 
-            var extractCommand = new Command("extract", "Extract all bitmaps from a WAD3 file");
+            var extractCommand = new Command("extract", "Extract all textures from a WAD3 file to PNG");
             rootCommand.AddCommand(extractCommand);
             var extractSourceArgument = new Argument<string>("inputWAD", "Path to WAD file to extract");
             var extractOutputArgument = new Argument<string>("outputFolder", "Path to folder to extract bitmaps to. (Will be created if non-existant)");
@@ -41,7 +48,11 @@ namespace Wad3CLI
 
             extractCommand.SetHandler((string inputWAD, string destFolder) =>
             {
-                Console.WriteLine($"WAD successfully extracted to {destFolder}");
+                if (!Directory.Exists(destFolder)) Directory.CreateDirectory(destFolder);
+                if (Extract(inputWAD, destFolder))
+                {
+                    Console.WriteLine($"WAD successfully extracted to {destFolder}");
+                }
             }, extractSourceArgument, extractOutputArgument);
             rootCommand.Invoke(args);
         }
@@ -52,44 +63,40 @@ namespace Wad3CLI
         {
             // Create wad
             WadFile wad = new WadFile(Sledge.Formats.Texture.Wad.Version.Wad3);
-            PnnQuant.PnnQuantizer quantizer = new PnnQuant.PnnQuantizer();
             foreach (var imagePath in bitmapsPath)
             {
                 string textureName = Path.GetFileNameWithoutExtension(imagePath);
                 if (textureName.Length > 16) textureName = textureName.Substring(0, 16);
                 MipTextureLump mipTexLump = new MipTextureLump();
+                ColorPalette palette = new ColorPalette(256);
                 mipTexLump.Name = textureName;
                 mipTexLump.NumMips = 4;
                 mipTexLump.MipData = new byte[mipTexLump.NumMips][];
                 // Set properties
-                using (Bitmap bitmap = new Bitmap(imagePath))
+                SKBitmap bitmap = SKBitmap.Decode(imagePath);
+                mipTexLump.Width = (uint)bitmap.Width;
+                mipTexLump.Height = (uint)bitmap.Height;
+                // Quantize image (erode image to 256 colors)
+                bitmap = bitmap.ConvertPixelFormat(OptimizedPaletteQuantizer.MedianCut(256));
+                // Process image 4 times, reducing size each time (original, /2, /4, /8)
+                for (int mipLevel = 0; mipLevel < 4; mipLevel++)
                 {
-                    mipTexLump.Width = (uint)bitmap.Size.Width;
-                    mipTexLump.Height = (uint)bitmap.Size.Height;
-                    // Quantize image (erode image to 256 colors)
-                    using (Bitmap qImage = quantizer.QuantizeImage(bitmap, PixelFormat.Format8bppIndexed, 255, true))
+                    SKSizeI mipSize = new SKSizeI((int)(mipTexLump.Width / MipDivisors[mipLevel]), (int)(mipTexLump.Height / MipDivisors[mipLevel]));
+                    SKBitmap mipImage = mipLevel > 0 ? bitmap.Resize(mipSize, SKFilterQuality.None) : bitmap;
+                    byte[] mipData = new byte[mipSize.Width * mipSize.Height];
+                    // Load image
+                    for (int y = 0; y < mipSize.Height; y++)
                     {
-                        // Run image on 4 sizes, (original, /2, /4, /8)
-                        for (int mipLevel = 0; mipLevel < 4; mipLevel++)
+                        for (int x = 0; x < mipSize.Width; x++)
                         {
-                            Size mipSize = new Size((int)(mipTexLump.Width / MipDivisors[mipLevel]), (int)(mipTexLump.Height / MipDivisors[mipLevel]));
-                            Bitmap mipImage = mipLevel > 0 ? ResizeBitmap(qImage, mipSize) : qImage;
-                            byte[] mipData = new byte[mipSize.Width * mipSize.Height];
-                            // Load image
-                            for (int y = 0; y < mipSize.Height; y++)
-                            {
-                                for (int x = 0; x < mipSize.Width; x++)
-                                {
-                                    Color color = mipImage.GetPixel(x, y);
-                                    int paletteIndex = Array.FindIndex(qImage.Palette.Entries, c => (c.R == color.R && c.G == color.G && c.B == color.B));
-                                    mipData[(y * mipSize.Width) + x] = Convert.ToByte(paletteIndex);
-                                }
-                            }
-                            mipTexLump.MipData[mipLevel] = mipData;
+                            SKColor color = mipImage.GetPixel(x, y);
+                            int paletteIndex = palette.GetOrAddPaletteIndex(color);
+                            mipData[(y * mipSize.Width) + x] = Convert.ToByte(paletteIndex);
                         }
-                        mipTexLump.Palette = GetPaletteBytes(qImage);
                     }
+                    mipTexLump.MipData[mipLevel] = mipData;
                 }
+                mipTexLump.Palette = palette.GetBytes();
                 wad.AddLump(mipTexLump.Name, mipTexLump);
             }
             // Write
@@ -99,40 +106,35 @@ namespace Wad3CLI
             }
             return true;
         }
-        static void Extract(string inputWAD, string destFolder)
+        static bool Extract(string inputWAD, string destFolder)
         {
-        }
-
-        // Utils
-
-        static Bitmap ResizeBitmap(Bitmap source, Size newSize)
-        {
-            Bitmap result = new Bitmap(newSize.Width, newSize.Height);
-            using (Graphics g = Graphics.FromImage(result))
+            using (FileStream rfs = File.OpenRead(inputWAD))
             {
-                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-                g.DrawImage(source, 0, 0, newSize.Width, newSize.Height);
+                WadFile wad = new WadFile(rfs);
+                // Parse Textures
+                foreach (Entry entry in wad.Entries)
+                {
+                    if (entry.Type != LumpType.MipTexture) continue;
+                    MipTextureLump lump = (MipTextureLump)wad.GetLump(entry);
+                    // Parse lump colors
+                    SKColor[] colorPalette = new SKColor[lump.Palette.Length / 3];
+                    for (int i = 0; i < lump.Palette.Length; i += 3)
+                    {
+                        colorPalette[i / 3] = new SKColor(lump.Palette[i], lump.Palette[i + 1], lump.Palette[i + 2]);
+                    }
+                    SKBitmap bitmap = new SKBitmap((int)lump.Width, (int)lump.Height);
+                    for (int i = 0; i < lump.MipData[0].Length; i++)
+                    {
+                        bitmap.SetPixel((int)(i % lump.Width), (int)(i / lump.Width), colorPalette[lump.MipData[0][i]]);
+                    }
+                    // Write image
+                    using (FileStream wfs = File.OpenWrite(Path.Combine(destFolder, $"{lump.Name}.png")))
+                    {
+                        bitmap.Encode(wfs, SKEncodedImageFormat.Png, 100);
+                    }
+                }
             }
-            return result;
-        }
-
-        static byte[] GetPaletteBytes(Bitmap image)
-        {
-            byte[] paletteData = new byte[256 * 3];
-            int paletteIndex = 0;
-            foreach (Color color in image.Palette.Entries)
-            {
-                paletteData[paletteIndex] = color.R;
-                paletteData[paletteIndex + 1] = color.G;
-                paletteData[paletteIndex + 2] = color.B;
-                paletteIndex += 3;
-            }
-            // Pad missing bytes
-            while (paletteIndex < paletteData.Length)
-            {
-                paletteData[paletteIndex] = 0;
-            }
-            return paletteData;
+            return true;
         }
     }
 }
